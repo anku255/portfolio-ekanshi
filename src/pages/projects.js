556175fae763051441776a289f0c1a9f872c3828
Ankit Tiwari <ankitt255@gmail.com>
2020-04-14 23:13:05 +0530

    📝 WIP: Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react'
-import Helmet from 'react-helmet'
-import Layout from '../components/Layout'
-import { graphql, Link } from 'gatsby'
-import Img from 'gatsby-image'
-import Masonry from 'react-masonry-component'
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import Masonry from 'react-masonry-component';
+import Layout from '../components/Layout';
+import ArrowLink from '../components/ArrowLink';
+
+const getItemWidth = columns => {
+  switch (columns) {
+    case 2:
+      return '66.666%';
+    case 3:
+      return '100%';
+    default:
+      return '33.333%';
+  }
+};
 
 class Projects extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
-    const categories = this.props.data.categories.edges
+    const categories = this.props.data.categories.edges;
 
     return (
       <Layout>
@@ -20,33 +28,42 @@ class Projects extends Component {
         </Helmet>
         <div className="projects-container">
           <h1 className="heading-primary">Projects</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolorem similique nemo et maxime sequi voluptas dolores incidunt maiores eligendi?</p>
-    {/* <pre>{JSON.stringify(categories, null, 2)}</pre> */}
-          {
-            categories.map(({node: category}) => {
-              return (
-                <Masonry className="showcase">
-                  {category.projects.map(project => (
-                    <div key={project.id} className="showcase__item">
-                      <Link to={`/image/${project.slug}`}>
-                        <Img fluid={project.coverImage.fluid} />
-                        {/* <h4 className="showcase__title">
-                          <span className="showcase__title-span">{project.title}</span>
-                        </h4> */}
-                      </Link>
-                    </div>
-                  ))}
-                </Masonry>
-              )
-            })
-          }
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolorem similique nemo et maxime sequi voluptas
+            dolores incidunt maiores eligendi?
+          </p>
+          {categories.map(({ node: category }) => (
+            <Masonry className="showcase">
+              {category.projects &&
+                category.projects.map(project => (
+                  <div key={project.id} className="showcase__item" style={{ width: getItemWidth(project.columns) }}>
+                    <Link to={`/image/${project.slug}`}>
+                      <Img fluid={project.coverImage.fluid} />
+                      <div
+                        className="showcase__title__wrap"
+                        onMouseEnter={e => {
+                          e.target.style.background = category.backgroundColor;
+                        }}
+                        style={{
+                          color: category.titleColor ? category.titleColor : '#252836',
+                        }}
+                      >
+                        <div className="showcase__title">{project.title}</div>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+            </Masonry>
+          ))}
         </div>
+        <ArrowLink position="left" to="/" label="Home" />
+        <ArrowLink position="right" to="about" label="About" />
       </Layout>
-    )
+    );
   }
 }
 
-export default Projects
+export default Projects;
 
 export const pageQuery = graphql`
   query {
@@ -56,13 +73,16 @@ export const pageQuery = graphql`
         node {
           id
           name
+          backgroundColor
+          titleColor
           projects {
             id
             slug
             title
+            columns
             coverImage {
-              fluid(maxWidth: 200) {
-              ...GatsbyContentfulFluid_tracedSVG
+              fluid(maxWidth: 800) {
+                ...GatsbyContentfulFluid
               }
             }
           }
@@ -70,4 +90,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
